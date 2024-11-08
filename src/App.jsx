71a30e4fde0480eb53f0cee8b39e4a7165dcc3b6
Deepanshu Sharma <sharmadeepanshu9901@gmail.com>
2024-11-08 20:59:57 +0530

    deep 9

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 import Home from "./pages/Home/Home";
 
 import Navbar from './components/Navbar/Navbar';
-// import JournalsPage from './pages/Journals/journals.jsx';
+import JournalsPage from './pages/Journals/journals.jsx';
 import Footer from './components/Footer/Footer';
 import About from './pages/About/About';
 import Games from './pages/Games/Games';
@@ -26,7 +26,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/games' element={<Games />}/>
-        {/* <Route path='/journals' element={<JournalsPage/>}/> */}
+        <Route path='/journals' element={<JournalsPage/>}/>
         <Route path='/quiz' element={<Quiz/>}/>
         <Route path='/contact' element={<Contact/>}/>
         </Routes>

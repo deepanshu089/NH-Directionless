86c7c29de25bed3021e752d00fa662eae9642e81
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2024-11-08 18:16:47 +0530

    FIfth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from "./pages/Home/Home";
-import Navbar from './components/Navbar/Navbar';
+
 import Footer from './components/Footer/Footer';
 import About from './pages/About/About';
 
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Navbar />
+      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />

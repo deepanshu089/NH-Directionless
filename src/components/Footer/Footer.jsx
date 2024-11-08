3c9c4c8a38853d7b76941fba7a091a7882a163e2
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2024-11-08 18:13:30 +0530

    Fourth Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,21 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      Footer
+      <div className={styles.footersection}>
+        <h3>About Us</h3>
+        <p>Learn more about our mission and vision.</p>
+    </div>
+    <div className={styles.footersection}>
+        <h3>Quick Links</h3>
+        <a href="#">Home</a> |
+        <a href="#">Services</a> |
+        <a href="#">Contact</a>
+    </div>
+    <div className={styles.footersection}>
+        <h3>Contact Us</h3>
+        <p>Email: contact@example.com</p>
+        <p>Phone: (123) 456-7890</p>
+    </div>
     </div>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import ReactConfetti from "react-confetti";
+import styles from "./Hero.module.scss";
+
+function Hero() {
+  const [windowDimension, setDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const detectSize = () => {
+    setDimension((prev) => ({
+      ...prev,
+      width: window.innerWidth,
+    }));
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", detectSize);
+    return () => {
+      window.removeEventListener("resize", detectSize);
+    };
+  }, [windowDimension]);
+
+  return (
+    <>
+      <div className={styles.hero}>
+        <ReactConfetti
+          width={windowDimension.width}
+          height={windowDimension.height}
+        />
+        <div className={styles.balloon} />
+        <div className={styles.balloon} />
+        <div className={styles.balloon} />
+        <div className={styles.balloon} />
+        <div className={styles.balloon} />
+        <div className={styles.balloon} />
+        <h1>Welcome to Pooh's Paradise</h1>
+      </div>
+    </>
+  );
+}
+
+export default Hero;

6f6fbd01bb13688eb51d9aa788dada17ed1bf471
Deepanshu Sharma <sharmadeepanshu9901@gmail.com>
2024-11-09 18:59:14 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
 import Home from "./pages/Home/Home";
 import SchedulerForm from './components/Scheduler/SchedulerForm';
 import ScheduleGenerate from './components/Scheduler/ScheduleGenerate';
@@ -11,23 +13,29 @@ import Games from './pages/Games/Games';
 import Quiz from './pages/Games/quiz';
 import Contact from './pages/Contacts/Contacts';
 import Chatbot from './pages/Chatbot/Chatbot';
-
+import { useState } from 'react';
 
 function App() {
+  const [studyPlan, setStudyPlan] = useState(null);
+
+  const handleGeneratePlan = (data) => {
+    setStudyPlan(data);
+  };
+
   return (
     <>
       <Navbar />
-
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/games' element={<Games />}/>
-        <Route path='/journals' element={<JournalsPage/>}/>
-        <Route path='/quiz' element={<Quiz/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/games' element={<Games />} />
+        <Route path='/journals' element={<JournalsPage />} />
+        <Route path='/quiz' element={<Quiz />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='/talk' element={<Chatbot/>}/>
-
-        </Routes>
+        <Route path='/scheduler' element={<SchedulerForm onGenerate={handleGeneratePlan} />} />
+        <Route path='/schedule-generate' element={<ScheduleGenerate studyPlan={studyPlan} />} />
+      </Routes>
       <Footer />
     </>
   );

4ec10f10755d74c5112acba71b0e08eda0669935
Deepanshu Sharma <sharmadeepanshu9901@gmail.com>
2024-11-09 20:36:47 +0530

    final7

--- src/pages/Games/Games.jsx
+++ src/pages/Games/Games.jsx
@@ -49,7 +49,7 @@ const Games = () => {
       </div>
 
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-6xl w-full px-4 md:px-0">
+      <div className="grid grid-cols-1 overflow-y-hidden sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-6xl w-full px-4 md:px-0">
         {gamePosters.map((poster) => (
           <div
             key={poster.id}
